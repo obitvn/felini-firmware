7070d8d3bbd148285f52f793fd1484e97d4ba8b7
ChuTienThinh <tienthinh.obit@gmail.com>
2023-08-15 16:24:50 +0700

    change gui analog

--- main/App/Pages/AnalogViewer/AnalogViewerView.cpp
+++ main/App/Pages/AnalogViewer/AnalogViewerView.cpp
@@ -10,59 +10,80 @@ void AnalogViewerView::Create(lv_obj_t *root)
     /*Change the active screen's background color*/
     lv_obj_set_style_bg_color(root, lv_color_hex(0), LV_PART_MAIN);
 
-    lv_obj_t *ui_AppLabel = lv_label_create(root);
-    lv_obj_set_width(ui_AppLabel, LV_SIZE_CONTENT);  /// 1
-    lv_obj_set_height(ui_AppLabel, LV_SIZE_CONTENT); /// 1
-    lv_obj_set_x(ui_AppLabel, -72);
-    lv_obj_set_y(ui_AppLabel, -89);
-    lv_obj_set_align(ui_AppLabel, LV_ALIGN_CENTER);
-    lv_label_set_text(ui_AppLabel, "12.3V");
-    lv_obj_set_style_text_color(ui_AppLabel, lv_color_hex(0x02DBF8), LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_text_opa(ui_AppLabel, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_text_font(ui_AppLabel, ResourcePool::GetFont("alibabasans_regular_48"), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_t *ui_Chart1 = lv_chart_create(root);
+    lv_obj_set_width(ui_Chart1, 216);
+    lv_obj_set_height(ui_Chart1, 124);
+    lv_obj_set_x(ui_Chart1, 3);
+    lv_obj_set_y(ui_Chart1, 1);
+    lv_obj_set_align(ui_Chart1, LV_ALIGN_CENTER);
+    lv_chart_set_type(ui_Chart1, LV_CHART_TYPE_LINE);
+    lv_chart_set_range(ui_Chart1, LV_CHART_AXIS_PRIMARY_Y, 0, 20);
+    lv_chart_set_div_line_count(ui_Chart1, 10, 10);
+    lv_chart_set_zoom_x(ui_Chart1, 255);
+    lv_chart_set_zoom_y(ui_Chart1, 255);
+    lv_chart_set_axis_tick(ui_Chart1, LV_CHART_AXIS_PRIMARY_X, 10, 5, 5, 2, true, 50);
+    lv_chart_set_axis_tick(ui_Chart1, LV_CHART_AXIS_PRIMARY_Y, 10, 2, 3, 2, true, 50);
+    lv_chart_set_axis_tick(ui_Chart1, LV_CHART_AXIS_SECONDARY_Y, 10, 5, 5, 2, false, 25);
+    lv_chart_series_t *ui_Chart1_series_1 = lv_chart_add_series(ui_Chart1, lv_color_hex(0x808080),
+                                                                LV_CHART_AXIS_PRIMARY_Y);
+    static lv_coord_t ui_Chart1_series_1_array[] = {0, 3, 5, 8, 9, 12, 15, 4, 6, 9, 12, 22, 19, 18, 16};
+    lv_chart_set_ext_y_array(ui_Chart1, ui_Chart1_series_1, ui_Chart1_series_1_array);
 
-    lv_obj_t *ui_VoltMax = lv_label_create(root);
-    lv_obj_set_width(ui_VoltMax, LV_SIZE_CONTENT);  /// 1
-    lv_obj_set_height(ui_VoltMax, LV_SIZE_CONTENT); /// 1
-    lv_obj_set_x(ui_VoltMax, 36);
-    lv_obj_set_y(ui_VoltMax, -101);
-    lv_obj_set_align(ui_VoltMax, LV_ALIGN_CENTER);
-    lv_label_set_text(ui_VoltMax, "Analog");
-    lv_obj_set_style_text_color(ui_VoltMax, lv_color_hex(0xFB0A0A), LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_text_opa(ui_VoltMax, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_text_font(ui_VoltMax, ResourcePool::GetFont("alibabasans_regular_16"), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_t *ui_Label6 = lv_label_create(root);
+    lv_obj_set_width(ui_Label6, LV_SIZE_CONTENT);  /// 1
+    lv_obj_set_height(ui_Label6, LV_SIZE_CONTENT); /// 1
+    lv_obj_set_x(ui_Label6, 120);
+    lv_obj_set_y(ui_Label6, -82);
+    lv_obj_set_align(ui_Label6, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_Label6, "mV");
+    lv_obj_set_style_text_color(ui_Label6, lv_color_hex(0x42FC10), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_Label6, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_Label6, &lv_font_montserrat_14, LV_PART_MAIN | LV_STATE_DEFAULT);
 
-    lv_obj_t *ui_VoltRealTime = lv_label_create(root);
-    lv_obj_set_width(ui_VoltRealTime, LV_SIZE_CONTENT);  /// 1
-    lv_obj_set_height(ui_VoltRealTime, LV_SIZE_CONTENT); /// 1
-    lv_obj_set_x(ui_VoltRealTime, 36);
-    lv_obj_set_y(ui_VoltRealTime, -74);
-    lv_obj_set_align(ui_VoltRealTime, LV_ALIGN_CENTER);
-    lv_label_set_text(ui_VoltRealTime, "10.145 mV");
-    lv_obj_set_style_text_color(ui_VoltRealTime, lv_color_hex(0x27FB0A), LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_text_opa(ui_VoltRealTime, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_text_font(ui_VoltRealTime, ResourcePool::GetFont("alibabasans_regular_16"), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_t *ui_Label8 = lv_label_create(root);
+    lv_obj_set_width(ui_Label8, LV_SIZE_CONTENT);  /// 1
+    lv_obj_set_height(ui_Label8, LV_SIZE_CONTENT); /// 1
+    lv_obj_set_x(ui_Label8, -1);
+    lv_obj_set_y(ui_Label8, 106);
+    lv_obj_set_align(ui_Label8, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_Label8, "connection to power out port");
+    lv_obj_set_style_text_color(ui_Label8, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_Label8, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_Label8, &lv_font_montserrat_12, LV_PART_MAIN | LV_STATE_DEFAULT);
 
-    lv_obj_t *ui_VoltChart = lv_obj_create(root);
-    lv_obj_set_width(ui_VoltChart, 257);
-    lv_obj_set_height(ui_VoltChart, 132);
-    lv_obj_set_x(ui_VoltChart, -1);
-    lv_obj_set_y(ui_VoltChart, 10);
-    lv_obj_set_align(ui_VoltChart, LV_ALIGN_CENTER);
-    lv_obj_clear_flag(ui_VoltChart, LV_OBJ_FLAG_SCROLLABLE); /// Flags
-    lv_obj_set_style_bg_color(ui_VoltChart, lv_color_hex(0x090909), LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_bg_opa(ui_VoltChart, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_border_color(ui_VoltChart, lv_color_hex(0x0FEFF3), LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_border_opa(ui_VoltChart, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_t *ui_Label5 = lv_label_create(root);
+    lv_obj_set_width(ui_Label5, LV_SIZE_CONTENT);  /// 1
+    lv_obj_set_height(ui_Label5, LV_SIZE_CONTENT); /// 1
+    lv_obj_set_x(ui_Label5, 42);
+    lv_obj_set_y(ui_Label5, -91);
+    lv_obj_set_align(ui_Label5, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_Label5, "10.294");
+    lv_obj_set_style_text_color(ui_Label5, lv_color_hex(0xFF6610), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_Label5, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_Label5, &lv_font_montserrat_40, LV_PART_MAIN | LV_STATE_DEFAULT);
 
-    lv_obj_t *ui_Pinmapvolt = lv_img_create(root);
-    lv_img_set_src(ui_Pinmapvolt, ResourcePool::GetImage("sd_card"));
-    lv_obj_set_width(ui_Pinmapvolt, LV_SIZE_CONTENT);  /// 1
-    lv_obj_set_height(ui_Pinmapvolt, LV_SIZE_CONTENT); /// 1
-    lv_obj_set_x(ui_Pinmapvolt, -1);
-    lv_obj_set_y(ui_Pinmapvolt, 104);
-    lv_obj_set_align(ui_Pinmapvolt, LV_ALIGN_CENTER);
-    lv_obj_add_flag(ui_Pinmapvolt, LV_OBJ_FLAG_ADV_HITTEST);
+    lv_obj_t *ui_Label7 = lv_label_create(root);
+    lv_obj_set_width(ui_Label7, LV_SIZE_CONTENT);  /// 1
+    lv_obj_set_height(ui_Label7, LV_SIZE_CONTENT); /// 1
+    lv_obj_set_x(ui_Label7, -83);
+    lv_obj_set_y(ui_Label7, -91);
+    lv_obj_set_align(ui_Label7, LV_ALIGN_CENTER);
+    lv_label_set_text(ui_Label7, "ADC");
+    lv_obj_set_style_text_color(ui_Label7, lv_color_hex(0x08A3FB), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_Label7, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_font(ui_Label7, &lv_font_montserrat_40, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    lv_obj_t *ui_Panel2 = lv_obj_create(root);
+    lv_obj_set_width(ui_Panel2, 2);
+    lv_obj_set_height(ui_Panel2, 42);
+    lv_obj_set_x(ui_Panel2, -29);
+    lv_obj_set_y(ui_Panel2, -91);
+    lv_obj_set_align(ui_Panel2, LV_ALIGN_CENTER);
+    lv_obj_clear_flag(ui_Panel2, LV_OBJ_FLAG_SCROLLABLE); /// Flags
+    lv_obj_set_style_bg_color(ui_Panel2, lv_color_hex(0x0BF429), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_opa(ui_Panel2, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_border_color(ui_Panel2, lv_color_hex(0x0BF429), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_border_opa(ui_Panel2, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
 }
 
 
